872328c68ce7b7a1c94970da99bf90819640a093
nosov <nosov@informika.ru>
2012-09-04 14:25:47 +0400

    add Admin default show updatedAt

--- CoreBundle/Admin/Admin.php
+++ CoreBundle/Admin/Admin.php
@@ -3,6 +3,7 @@
 namespace Iphp\CoreBundle\Admin;
 
 use Sonata\AdminBundle\Admin\Admin as BaseAdmin;
+use Knp\Menu\ItemInterface as MenuItemInterface;
 
 class Admin extends BaseAdmin
 {
@@ -24,6 +25,44 @@ class Admin extends BaseAdmin
     protected function getCurrentUser()
     {
         return $this->getConfigurationPool()->getContainer()->get('security.context')->getToken()->getUser();
+    }
+
+    /**
+     * @param \Knp\Menu\ItemInterface $menu
+     * @param $action
+     * @param null|\Sonata\AdminBundle\Admin\Admin $childAdmin
+     *
+     * @return void
+     */
+    protected function configureSideMenu(MenuItemInterface $menu, $action, BaseAdmin $childAdmin = null)
+    {
+        if (!$childAdmin && !in_array($action, array('edit'))) {
+            return;
+        }
+        $admin = $this->isChild() ? $this->getParent() : $this;
+
+        if (method_exists($admin->getSubject(), 'getCreatedAt'))
+        {
+            $menu->addChild($this->trans('Created At').':');
+            $menu->addChild( $admin->getSubject()->getCreatedAt()->format ('d.m.Y H:i:s'));
+        }
+
+        if (method_exists($admin->getSubject(), 'getUpdatedAt'))
+        {
+            $menu->addChild($this->trans('Updated At').':');
+            $menu->addChild( $admin->getSubject()->getUpdatedAt()->format ('d.m.Y H:i:s'));
+        }
+
+
+        if (method_exists($admin->getSubject(), 'getSitePath'))
+            $menu->addChild(
+                $this->trans('View on site'),
+                array('uri' => $admin->getSubject()->getSitePath(), 'linkAttributes' => array('target' => '_blank'))
+            );
+
+
+
+
     }
 
 }
